afddbe344f4ff4077604a85f1127b7569b102d41
jonmoon74 <jon.moon@outlook.com>
2018-06-09 20:27:34 +0100

    task class built and test data ready on first view

---
+++ DoIt/Task.swift
@@ -0,0 +1,15 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Jon Moon on 09/06/2018.
+//  Copyright © 2018 Jon Moon. All rights reserved.
+//
+
+import Foundation
+
+
+class Task {
+    var name = ""
+    var important = false
+}
