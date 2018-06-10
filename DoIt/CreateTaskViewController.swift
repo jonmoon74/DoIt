d14f019d16c44968ba5b7f5ca2187aaf2bbcafa9
jonmoon74 <jon.moon@outlook.com>
2018-06-10 21:11:56 +0100

    Renaming of initial viewController, plus all the code for the createTasksViewController

---
+++ DoIt/CreateTaskViewController.swift
@@ -0,0 +1,49 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt
+//
+//  Created by Jon Moon on 10/06/2018.
+//  Copyright © 2018 Jon Moon. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    var previousVC = TasksViewController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+        //create a task and add it to the array
+        let task = Task()
+        task.name = taskNameTextField.text!
+        task.important = importantSwitch.isOn
+        
+        previousVC.tasks.append(task)
+        previousVC.tableView.reloadData()
+        navigationController!.popViewController(animated: true)
+        
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
